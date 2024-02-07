781f4747ff8af5f23a23863f36337252c92332ae
胡海 <huhai@mypy.cn>
2024-02-07 14:41:17 +0800

    feat(): 初始化项目

---
+++ .dumi/theme/slots/Footer/index.tsx
@@ -0,0 +1,5 @@
+import {Footer} from 'dumi-theme-antd-style';
+
+export default () => {
+  return <Footer bottom={'自定义'} columns={[]}/>;
+};
--- .dumirc.ts
+++ .dumirc.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   themeConfig: {
     name: 'Zx-Dumi',
+    footer: `<p>Open-source MIT Licensed | Copyright © 2024-present</p><p>Powered by ZheXian</p>`,
   },
 });
