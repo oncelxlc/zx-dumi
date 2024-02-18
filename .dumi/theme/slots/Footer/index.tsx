50028244c76bb38bc4842d67474f603de4d22592
胡海 <huhai@mypy.cn>
2024-02-18 11:30:53 +0800

    feat(): 添加react依赖

--- .dumi/theme/slots/Footer/index.tsx
+++ .dumi/theme/slots/Footer/index.tsx
@@ -1,5 +1,6 @@
+import React from 'react';
 import {Footer} from 'dumi-theme-antd-style';
 
 export default () => {
-  return <Footer bottom={'自定义'} columns={[]}/>;
+  return <Footer bottom={'自定义测试'} columns={[]}/>;
 };
